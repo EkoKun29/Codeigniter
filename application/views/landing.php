cd96309bde18dca05a808f10671d4451fc36445d
ridwan <ridwan@patikab.go.id>
2021-03-31 10:42:56 +0700

    delete db

--- application/views/landing.php
+++ application/views/landing.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Regna Bootstrap Template</title>
+    <title>Aplikasi Desk TIK</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="<?= base_url('data modul/Regna/'); ?>assets/img/favicon.png" rel="icon">
-    <link href="<?= base_url('data modul/Regna/'); ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="<?= base_url(''); ?>img/logo_circle.png" rel="icon">
+    <!-- <link href="<?= base_url(''); ?>/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
